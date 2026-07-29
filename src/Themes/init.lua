08fba7f9a076dbcecbaa275b5037848d1b47ea3e
Khoim123 <lethanhkhoi4408@gmail.com>
2026-07-29 17:10:33 +0700

    Update init.lua

--- src/Themes/init.lua
+++ src/Themes/init.lua
@@ -1,18 +1,19 @@
 local Themes = {
-	Names = {
-		"Dark",
-		"Darker",
-		"Light",
-		"Aqua",
-		"Amethyst",
-		"Rose",
-   "Emerald",
-	},
+    Names = {
+        "Dark",
+        "Darker",
+        "Light",
+        "Aqua",
+        "Amethyst",
+        "Rose",
+        "Emerald",
+    },
 }
 
 for _, Theme in next, script:GetChildren() do
-	local Required = require(Theme)
-	Themes[Required.Name] = Required
+    if Theme:IsA("ModuleScript") then
+        Themes[Theme.Name] = require(Theme)
+    end
 end
 
 return Themes
