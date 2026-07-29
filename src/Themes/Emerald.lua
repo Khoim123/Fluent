919bba4d46ab479cbbd4e4801f8aa8885864a60c
Khoim123 <lethanhkhoi4408@gmail.com>
2026-07-29 17:00:20 +0700

    Create Emerald.lua

---
+++ src/Themes/Emerald.lua
@@ -0,0 +1,47 @@
+return {
+    Name = "Emerald",
+    Accent = Color3.fromRGB(16, 185, 129),
+
+    AcrylicMain = Color3.fromRGB(30, 50, 42),
+    AcrylicBorder = Color3.fromRGB(50, 78, 62),
+    AcrylicGradient = ColorSequence.new(Color3.fromRGB(20, 38, 30), Color3.fromRGB(20, 38, 30)),
+    AcrylicNoise = 0.9,
+
+    TitleBarLine = Color3.fromRGB(42, 68, 54),
+    Tab = Color3.fromRGB(72, 105, 86),
+
+    Element = Color3.fromRGB(72, 105, 86),
+    ElementBorder = Color3.fromRGB(16, 32, 22),
+    InElementBorder = Color3.fromRGB(50, 78, 62),
+    ElementTransparency = 0.87,
+
+    ToggleSlider = Color3.fromRGB(72, 105, 86),
+    ToggleToggled = Color3.fromRGB(0, 0, 0),
+
+    SliderRail = Color3.fromRGB(72, 105, 86),
+
+    DropdownFrame = Color3.fromRGB(105, 145, 118),
+    DropdownHolder = Color3.fromRGB(26, 48, 38),
+    DropdownBorder = Color3.fromRGB(16, 32, 22),
+    DropdownOption = Color3.fromRGB(72, 105, 86),
+
+    Keybind = Color3.fromRGB(72, 105, 86),
+
+    Input = Color3.fromRGB(105, 145, 118),
+    InputFocused = Color3.fromRGB(6, 16, 10),
+    InputIndicator = Color3.fromRGB(95, 135, 108),
+
+    Dialog = Color3.fromRGB(26, 48, 38),
+    DialogHolder = Color3.fromRGB(16, 32, 22),
+    DialogHolderLine = Color3.fromRGB(12, 26, 18),
+    DialogButton = Color3.fromRGB(26, 48, 38),
+    DialogButtonBorder = Color3.fromRGB(50, 78, 62),
+    DialogBorder = Color3.fromRGB(42, 68, 54),
+    DialogInput = Color3.fromRGB(36, 58, 46),
+    DialogInputLine = Color3.fromRGB(105, 145, 118),
+
+    Text = Color3.fromRGB(240, 240, 240),
+    SubText = Color3.fromRGB(140, 182, 155),
+    Hover = Color3.fromRGB(72, 105, 86),
+    HoverChange = 0.07,
+}
